e0d80b01904311e0f1132872f0bb828a3021b116
etph78 <42941423+etph78@users.noreply.github.com>
2024-06-25 21:08:49 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from io import BytesIO
 st.title("✨ Noodle Coins app")
 
 # xlsx_file_init = r'https://www.bankhapoalim.co.il/sites/default/files/media/DohotKaspiem/120012411.xlsx'
-xlsx_file_init = r'C:\Temp\to_delete\\-.xlsx'
+xlsx_file_init = r'C:\Temp\to_delete\-.xlsx'
 # xlsx_file_init = 'https://noodle-coins.streamlit.app/'
 data = {
     "Questions": [0, 1, 2, 3],
